6d404e8df0676e097a8d58622d52ae61a0616c2f
Braz <jonatasbraz@gmail.com>
2019-06-14 21:34:32 -0300

    backend finalizado

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -12,35 +12,32 @@ module.exports = {
     async store(req, res) {
         const { author, place, description , hatshtags } = req.body;
         const { filename: image } = req.file;
+        const [ name ] = image.split('.')
+        const fileName = `${name}.jpg`;
 
         await sharp(req.file.path)
             .resize(500)
             .jpeg({ quality: 70 })
             .toFile(
-                path.resolve( req.file.destination, 'resized', image)
+                path.resolve( req.file.destination, 'resized', fileName)
             );
 
-        try {
-            fs.unlinkSync(req.file.path, function(err){
-                if(err){
-                    console.log('teste');
-                    console.log(err);
-                } else {
-                  console.log('doneaswell');
-                }
-              });
-        } catch(e) {
-            console.log("tratada");
-            console.error(e);
-        }
+        // try {
+            fs.unlinkSync(req.file.path);
+        // } catch(e) {
+        //     console.error(e);
+        // }
 
         const post = await Post.create({
             author,
             place,
             description,
             hatshtags,
-            image
+            image: fileName
         });
+
+        req.io.emit('post', post);
+
         return res.json(post);
     }
 };
